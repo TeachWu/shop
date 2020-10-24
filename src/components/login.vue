33ea006dff96b7440c93792f3bc33f0221ac45d0
吴亚朝 <1229669495@qq.com>
2020-10-24 14:15:15 +0800

    完成了登录

---
+++ src/components/login.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="login_container">
+    <div class="mid_container">
+      <!--头像-->
+      <div class="avater_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <!--表单-->
+      <el-form ref="loginFormRef" class="login_form" :rules="loginFormRules" :model="loginForm">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data() {
+      return {
+        loginForm: {
+          username: 'admin',
+          password: '123456'
+        },
+        loginFormRules: {
+          username: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+          ]
+
+        }
+      }
+
+    },
+    methods: {
+      resetLoginForm() {
+        this.$refs.loginFormRef.resetFields()
+      },
+      login() {
+        this.$refs.loginFormRef.validate(async (valid) => {
+          if (!valid) return
+          const { data: res } = await this.$http.post('login', this.loginForm)
+          if (res.meta.status !== 200) return this.$message.error('登录失败')
+          this.$message.success('登录成功')
+          console.log(res)
+          window.sessionStorage.setItem('token', res.data.token)
+          this.$router.push('/home')
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+  }
+
+  .mid_container {
+    background-color: #FFF;
+    height: 300px;
+    width: 450px;
+    border-radius: 5px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .avater_box {
+      height: 130px;
+      width: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 10px;
+      border-shadow: 0 0 10px #ddd;
+      position: absolute;
+      left: 50%;
+      background-color: #fff;
+      transform: translate(-50%, -50%);
+      img {
+        background-color: #eee;
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+      }
+    }
+  }
+
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+
+  }
+
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+</style>
